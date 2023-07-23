d52d0dfd22342b6c172f4da6471f3bf9353ab2e9
andbudha <andreibartov@gmail.com>
2023-07-24 01:21:33 +0200

    AppContainer updated/info containers added

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,4 +1,5 @@
 import { BiSearch } from "react-icons/bi";
+import { CiLocationOn } from "react-icons/ci";
 import styles from './AppContainer.module.scss';
 import { ChangeEvent, useState } from "react";
 
@@ -33,6 +34,20 @@ export const AppContainer = () => {
                         < BiSearch className={styles.search_icon} />
                     </div>
                 </div>
+                <div className={styles.city}>
+                    <div className={styles.location_icon}><CiLocationOn className={styles.icon} /></div>
+                    <div className={styles.location_name}>
+                        <p className={styles.name}>Chisinau,</p>
+                    </div>
+                </div>
+                <div className={styles.country}>
+                    <p className={styles.name}>Moldova</p>
+                </div>
+                <div className={styles.temperature}>
+                    <p className={styles.degrees}>35</p>
+                    <p className={styles.symbol}>&#8451;</p>
+                </div>
+                <div className={styles.weather_details}></div>
             </div>
         </>
     );
