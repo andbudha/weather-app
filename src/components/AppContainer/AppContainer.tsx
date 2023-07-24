44b39b083ac08f745f7fe237e1bf054b887cd082
andbudha <andreibartov@gmail.com>
2023-07-24 12:29:23 +0200

    styles updated

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -1,4 +1,5 @@
 import { BiSearch } from "react-icons/bi";
+import { BsCloudy } from "react-icons/bs";
 import { CiLocationOn } from "react-icons/ci";
 import styles from './AppContainer.module.scss';
 import { ChangeEvent, useState } from "react";
@@ -34,8 +35,24 @@ export const AppContainer = () => {
                         < BiSearch className={styles.search_icon} />
                     </div>
                 </div>
+                <div className={styles.temperature}>
+                    <p className={styles.degrees}>35</p>
+                    <p className={styles.symbol}>&#8451;</p>
+                </div>
+                <div className={styles.weather}>
+                    <div className={styles.weather_icon}>
+                        <BsCloudy className={styles.icon} />
+                    </div>
+                    <div className={styles.weather_details}>
+                        <p className={styles.details}>cloudy</p>
+                    </div>
+
+                </div>
                 <div className={styles.city}>
-                    <div className={styles.location_icon}><CiLocationOn className={styles.icon} /></div>
+                    <div className={styles.location_icon}>
+                        <CiLocationOn className={styles.icon} />
+                    </div>
+
                     <div className={styles.location_name}>
                         <p className={styles.name}>Chisinau,</p>
                     </div>
@@ -43,11 +60,6 @@ export const AppContainer = () => {
                 <div className={styles.country}>
                     <p className={styles.name}>Moldova</p>
                 </div>
-                <div className={styles.temperature}>
-                    <p className={styles.degrees}>35</p>
-                    <p className={styles.symbol}>&#8451;</p>
-                </div>
-                <div className={styles.weather_details}></div>
             </div>
         </>
     );
